57bfb1ccba958c2ec2ed3b46a90fa59a2dded797
lmanyeki-debug <lydiahmanyeki@gmail.com>
2025-02-28 15:33:45 +0300

    Unit converter commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const input=document.getElementById("number");
 const selection=document.getElementById("unit");
 const button=document.querySelector(".button");
 const displayResults=document.getElementById("results");
+const errorContent=document.querySelector("#error-content")
 
 let units = ["mm","cm","dm","m","Dm","Hm","Km" ];
 
@@ -42,15 +43,40 @@ let results={};
 
 }
 
+
 button.onclick = function () {
   let value = input.value;
   let fromUnit = selection.value;
   let connector=`<span class="connector"> is equal to</span>`;
 
   let convertedValues = convertUnits(value, fromUnit);
+  
+  errorContent.innerHTML = "";
+  input.classList.remove("error");
+  selection.classList.remove("error");
 
+  if (value === "" && fromUnit === "") {
+    
+    input.classList.add("error");
+    errorContent.innerHTML = "<p class='error-message'>Please enter a value </p>";
+  } else if (value === "") {
+   
+    input.classList.add("error");
+    errorContent.innerHTML = "<p class='error-message'>Please enter a value</p>";
+  } else if (fromUnit === "") {
+    
+    selection.classList.add("error");
+    errorContent.innerHTML = "<p class='error-message'>Please select a base unit</p>";
+  } else {
+   
+    input.classList.remove("error");
+    selection.classList.remove("error");
+    
   displayResults.innerHTML = "";
   for (let unit in convertedValues) {
+   
     displayResults.innerHTML += `<p>${value} ${fromUnit} ${connector} ${convertedValues[unit]}  ${unit}</p>`;
   }
 };
+}
+
